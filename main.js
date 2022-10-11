8d1599ef5beacd93885cfc5c92b7c9ce8c3ae673
JoseLuisReySaavedraADSO <reysaavedra123@gmail.com>
2022-10-11 09:29:25 -0500

    Punto 2

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
 addEventListener("DOMContentLoaded", ()=>{
-    // Calcular una distancia
-    let x1 = Number(prompt("ingrese la abcsisa 1: ", 3));
-    let x2 = Number(prompt("ingrese la ordenada 1: ", 4));
-    let y1 = Number(prompt("ingrese la abcsisa 2: ", 3));
-    let y2 = Number(prompt("ingrese la ordenada 2: ", 4));
-    x  = Math.pow(x2-x1, 2)
-    y  = Math.pow(y2-y1, 2)
-    d  = Math.sqrt(x+y)
-    let res = (d<0)
-            ? "¡¡LA SOLUCION ES INDEFINIDA!!"
-            : `La distancia es ${d}`;
-
-        document.body.insertAdjacentHTML("beforeend", `<div class="estilo">El valor de la distancia es ${d}</div> `);
+    // Calcular una ecuacion
+    let total = 0
+    let desc = 0
+    for (let step = 0; step < 3; step++){
+        let p1 = Number(prompt("Cual fue el costo del producto? ", 2300))
+        c1 = p1-(p1*5/100)
+        i1 = p1-c1
+        total = c1 + total
+        desc = i1 + desc
+    }
+    for (let step = 0; step < 2; step++){
+        let p1 = Number(prompt("Cual fue el costo del producto? ", 2300))
+        c1 = p1-(p1*2/100)
+        i1 = p1-c1
+        total = c1 + total
+        desc = i1 + desc
+    }
+    document.body.insertAdjacentHTML("beforeend", `<div class="estilo">el costo de sus productos con el descuento es ${total}$, su descuento fue de ${desc}$</div> `);
 })
