b281ab8c23b388bd2ee5795651a5056997fcff8f
JoseLuisReySaavedraADSO <reysaavedra123@gmail.com>
2022-10-13 09:06:36 -0500

    Punto 5

--- main.js
+++ main.js
@@ -1,10 +1,5 @@
 addEventListener("DOMContentLoaded", ()=>{
     // Calcular una ecuacion
-    let llaves = 11500 * 5
-    let bomba=1168000
-    let pernos=87000*3
-    let vueltos=91000
-    total=llaves+bomba+pernos+vueltos
-    total1=llaves+bomba+pernos
-    document.body.insertAdjacentHTML("beforeend", `<div class="estilo">El total que pago por los productos:<b> ${total1}$</b><br>Entonces sabemos que traia: <b>${total}$</b></div> `);
+    
+    document.body.insertAdjacentHTML("beforeend", `<div class="estilo">la cantidad de harina que tiene que añadir a su preparacion es de 300g </div> `);
 })
