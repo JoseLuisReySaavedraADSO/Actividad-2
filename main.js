0a03736517eec96e494ed7a3078c3c6f9bbba9bb
JoseLuisReySaavedraADSO <reysaavedra123@gmail.com>
2022-10-13 09:50:43 -0500

    Punto 10

--- main.js
+++ main.js
@@ -1,8 +1,4 @@
 addEventListener("DOMContentLoaded", ()=>{
     // Calcular una ecuacion
-    let nombre = prompt("Ingrese el nombre del personal: ")
-    let apellido = prompt("Ingrese apellido del personal")
-    let año = Number(prompt("Ingrese el año de nacimiento: "))
-    edad = 2022 - año
-    document.body.insertAdjacentHTML("beforeend", `<div class="estilo">Nombre: ${nombre} ${apellido} <br> Edad: ${edad} </div> `);
+    document.body.insertAdjacentHTML("beforeend", `<div class="estilo">La frase al reves es: <br><b>!rorre y abeurp se odot noicamargorp al euq laugi la adiv aL</b></div> `);
 })
